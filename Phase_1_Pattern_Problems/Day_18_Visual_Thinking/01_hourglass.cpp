b87bf82afca295d39846f860bc9e2064b52b9205
Anurag Kumar <anurag1224kumar@gmail.com>
2026-05-11 19:47:08 +0530

    Phase 1 Day 18: Add hourglass pattern implementation

---
+++ Phase_1_Pattern_Problems/Day_18_Visual_Thinking/01_hourglass.cpp
@@ -0,0 +1,38 @@
+// ============================================================================
+// PROBLEM 1: Hourglass Pattern
+// ============================================================================
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    // Top half
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) cout << " ";
+        for (int j = 0; j < 2 * (n - i) - 1; j++) cout << "*";
+        cout << "\n";
+    }
+    
+    // Bottom half
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) cout << " ";
+        for (int j = 0; j < 2 * i - 1; j++) cout << "*";
+        cout << "\n";
+    }
+    
+    return 0;
+}
+
+/*
+Example (n=4):
+*******
+ *****
+  ***
+   *
+  ***
+ *****
+*******
+*/
